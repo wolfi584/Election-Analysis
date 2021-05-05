4d159c6a9d0f9b1975552d561e4c76830a2a730b
Diana Endo <52835218+wolfi584@users.noreply.github.com>
2021-05-04 21:00:12 -0500

    Delete Python_fstring.py

--- Python_fstring.py
+++
@@ -1,6 +0,0 @@
-voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}]
-for county_dict in voting_data:
-     print(county_dict['county'])
-     print(county_dict['registered_voters'])
-
-     
